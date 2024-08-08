4437a9e22a8bea173057473cdaf9436ad411c9bd
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-08-09 00:59:44 +0530

    SADARU-MD V1

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const axios = require('axios')
 const { File } = require('megajs')
 const prefix = '.'
 
-const ownerNumber = ['94701814946']
+const ownerNumber = ['94701814945']
 
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
