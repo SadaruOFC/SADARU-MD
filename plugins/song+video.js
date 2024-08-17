b5cb9db0dc5e499bd518abeabb6a67599b26359b
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-08-17 10:01:00 +0530

    SADARU-MD V1

---
+++ plugins/song+video.js
@@ -0,0 +1,94 @@
+const {cmd , commands} = require('../command')
+const fg = require('api-dylux')
+const yts = require('yt-search')
+
+cmd({
+    pattern: "song",
+    desc: "download songs",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+if(!q) return reply("Please give me a url or title")
+const search = await yts(q)
+const deta = search.videos[0];
+const url = deta.url 
+
+let desc= ` 
+*SADARU-MD Song Downloader*
+
+ > Title : ${deta.title}
+ > Description : ${deta.description}
+ > Time : ${deta.timestamp}
+ > Ago : ${deta.ago}
+ > Views : ${deta.views}
+
+>Powered by SADARU-MD
+
+`
+
+await conn.sendMessage(from,{image :{ url: deta.thumbnail},caption:desc},{quoted:mek});
+
+//downlod audio+ document
+
+let down = await fg.yta(url)
+let downloadUrl = down.dl_url
+
+//send audio message 
+await conn.sendMessage(from,{audio:{url:downloadUrl},mimetype:"audio/mpeg",caption :">Powered by SADARU-MD"},{quoted:mek})
+  
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
+
+//========video dl=======
+
+cmd({
+    pattern: "video",
+    desc: "download videos",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+if(!q) return reply("Please give me a url or title")
+const search = await yts(q)
+const deta = search.videos[0];
+const url = deta.url 
+
+let desc= `
+*SADARU-MD Video Downloader*
+
+ > Title : ${deta.title}
+ > Description : ${deta.description}
+ > Time : ${deta.timestamp}
+ > Ago : ${deta.ago}
+ > Views : ${deta.views}
+
+>Powered by SADARU-MD
+
+`
+
+await conn.sendMessage(from,{image :{ url: deta.thumbnail},caption:desc},{quoted:mek});
+
+//downlod video + document 
+
+let down = await fg.ytv(url)
+let downloadUrl = down.dl_url
+
+//send video  message 
+await conn.sendMessage(from,{video:{url:downloadUrl},mimetype:"video/mp4",caption :">Powered by SADARU-MD"},{quoted:mek})
+
+  
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
