f5b53660dc7f6405fd02f4f4ef20b91b9537516b
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-08-19 18:02:03 +0530

    SADARU-MD V1

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "qdEF2IpA#F87Sk6CpB_vs04g3MMrF7NdDM7ZmcFHoCBxz3TU5IQE",
+SESSION_ID: process.env.SESSION_ID || "x6NDzDbD#4bOJU-cJGxuETewH3IRwZvZSnWwXW0Uzj-iIW1NLTjU",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/ca80ac72e58dc131fd32c.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello user, I am SADARU-MD.I am alive now.",
 };
