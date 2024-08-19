f5b3c365f98dac9b8be319e9fcc9785b092ec530
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-08-19 22:26:52 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -12,18 +12,10 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
-let status = `
-┌───────────────────────
-
-├ ⏰ *Runtime:-* ${runtime(process.uptime())}
-├ 📟 *Ram usage:-* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
-├ ⚙️ *Platform:-* ${os.hostname()}
-├ 👨‍💻 *Owners:-* Sadaru
-├ 🧬 *Version:-* 1.0
-└───────────────────────
-
-> SADARU-MD WA BOT 2024
-
+let status = `*Uptime:*  ${runtime(process.uptime())}
+*Ram usage:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+*HostName:* ${os.hostname()}
+*Owner:* Prabath Kumara & Your name
 `
     
 return reply('${status}')
