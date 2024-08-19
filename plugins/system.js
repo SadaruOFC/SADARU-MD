1c15567c37105897070826b27f648beb073a4ebc
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-08-19 22:29:39 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -11,14 +11,12 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-
 let status = `*Uptime:*  ${runtime(process.uptime())}
 *Ram usage:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
 *HostName:* ${os.hostname()}
 *Owner:* Prabath Kumara & Your name
 `
-    
-return reply('${status}')
+    return reply('${status}')
 
 }catch(e){
 console.log(e)
