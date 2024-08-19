107e9a4361587f73a90ca52290c342817d1dc95c
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-08-19 21:37:46 +0530

    SADARU-MD V1

---
+++ plugins/system.js
@@ -0,0 +1,33 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+const os = require("os")
+const {runtime} = require('../lib/functions')
+
+cmd({
+    pattern: "system",
+    desc: "Check runtime, owner & more...",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+let status = `┌───────────────────────
+├ ⏰ *Runtime:-* ${runtime(process.uptime())}
+├ 📟 *Ram usage:-* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+├ ⚙️ *Platform:-* ${os.hostname()}
+├ 👨‍💻 *Owners:-* Sadaru
+├ 🧬 *Version:-* 1.0
+└───────────────────────
+
+> SADARU-MD WA BOT 2024
+`
+
+return reply('${status}')
+
+}catch(e){
+console.log(e)
+reply('${e}')
+
+}
+})
