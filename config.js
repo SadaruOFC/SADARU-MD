fdd10289f2cb75fc4914e87774fec88c6fb70c64
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-08-19 19:48:49 +0530

    SADARU-MD V1

--- config.js
+++ config.js
@@ -7,18 +7,6 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "x6NDzDbD#4bOJU-cJGxuETewH3IRwZvZSnWwXW0Uzj-iIW1NLTjU",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/ca80ac72e58dc131fd32c.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "Hello there 👋
-
-*🙌 I am SADARU-MD WhatsApp User Bot*
-
-
-🧑‍💻 _Owner:_ Sadaru
-
-📞 _Owner Number:_ +94701814946
-
-📌 _Group Link:_ https://chat.whatsapp.com/DTy4uNnbcO6FLkJJ0uIM1y
-
-🔗 _Github:_ https://github.com/Sadarulk/SADARU-MD
-
-> SADARU-MD WA BOT 2024",
+ALIVE_MSG: process.env.ALIVE_MSG || "Hello there 🙌 I am SADARU-MD WhatsApp User bot.",
+MENU_MSG: process.env.MENU_MSG || "* .alive",
 };
